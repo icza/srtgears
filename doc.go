af4ce17e6e42f7bf7f997e86d37a7947d12a5d69
icza <iczaaa@gmail.com>
2016-01-23 05:13:13 +0100

    Refactored cmd folder, added doc.go and subs.go (model).

---
+++ doc.go
@@ -0,0 +1,7 @@
+/*
+
+Package srtgears implements a subtitle engine for reading subtitile files,
+manipuliating / transforming them and then saving the result into another file.
+
+*/
+package srtgears
---
+++ subs.go
@@ -0,0 +1,8 @@
+/*
+
+This file defines contains types and utilities to model subtitles.
+
+*/
+
+package srtgears
+
