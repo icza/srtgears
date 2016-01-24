715092b159e4fc702db588c79165445d9d34ea8c
icza <iczaaa@gmail.com>
2016-01-24 13:19:01 +0100

    Started working on the web interface. Basic AppEngine project.

---
+++ web/sgw/sgw.go
@@ -0,0 +1,9 @@
+/*
+
+Package sgw is the backend logic for the online Srtgears.
+
+It processes form submit requests, and connects the data to the srtgears engine,
+and sends back the result.
+
+*/
+package sgw
